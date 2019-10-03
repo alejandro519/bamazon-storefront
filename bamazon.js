c5c25b5227a41920101eeca18bb2c178c2bf4930
Real Ghost <alejandro519@gmail.com>
2019-10-02 23:14:40 -0700

    Added a for loop that will check current inventory level in the database against the item chosen

--- bamazon.js
+++ bamazon.js
@@ -16,16 +16,16 @@ connection.connect(function(err) {
 });
 
 function readProducts() {
-  console.log("Selecting all products...\n");
+  console.log("Showing all products in the store...\n");
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
     // connection.end();
-    itemSelection(res);
+    itemSelection();
   });
 }
-function itemSelection(inventory) {
+function itemSelection() {
   console.log("\nWelcome to Bamazon! Get ready to choose an item.\n");
   inquirer
     .prompt([
@@ -37,26 +37,44 @@ function itemSelection(inventory) {
       {
         name: "itemqty",
         type: "number",
-        message: "How many would you like to purchase (enter a number)",
+        message: "How many would you like to purchase (enter a quantity)",
       },
     ]).then(function(answers) {
-      console.log("itemid: " + answers.itemid)
-      console.log(inventory)
-      var qty = inventory.stock_quantity;
-      console.log("current quantity: " + inventory.stock_quantity)
+      console.log("\nitemid: " + answers.itemid,  "\nitemqty: " + answers.itemqty);
+      // console.log(answers)
+      // var qty = inventory.stock_quantity;
+      // console.log("current quantity: " + inventory.stock_quantity)
+      queryItemsOnHand(answers);
     });
 }
 
-//query the database to see the qty of the item selected - use workbench to initiate query
-  //SELECT * FROM products WHERE id=1
-//check if the qty selected is greater than the stock_qty
-  // if qty selected is greater - display "insufficient qty"
+function queryItemsOnHand(answers) {
+  var query = connection.query("SELECT * FROM products WHERE ?", 
+    {
+      id: answers.itemid
+    }, 
+    function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+  });
 
-//Otherwise this means updating the SQL database to reflect the remaining quantity.
-//Once the update goes through, show the customer the total cost of their purchase.
+  // logs the actual query being run
+  // console.log(query.sql);
+  connection.end();
+}
+
+
+
+
+
+
+
+
 
 // function deleteProduct() {
-//   console.log("Deleting all strawberry icecream...\n");
+//   console.log("Checking current inventory of itemid " + itemid);
 //   connection.query(
 //     "SELECT * FROM products WHERE ?",
 //     {
@@ -70,3 +88,14 @@ function itemSelection(inventory) {
 //     }
 //   );
 // }
+
+
+
+//query the database to see the qty of the item selected - use workbench to initiate query
+  //SELECT * FROM products WHERE id=1
+//check if the qty selected is greater than the stock_qty
+  // if qty selected is greater - display "insufficient qty"
+
+//Otherwise this means updating the SQL database to reflect the remaining quantity.
+//Once the update goes through, show the customer the total cost of their purchase.
+
